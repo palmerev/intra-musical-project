51baa5ec03b6f21ff6c1f9a12713b673df7939a0
Evan Palmer <evjpalmer@gmail.com>
2015-07-28 14:04:44 -0700

    setup XMLHttpRequest to GET JSON from get_interval_set view

--- django/intra_musical_project/ear_training_app/static/ear_training_app/exercise.js
+++ django/intra_musical_project/ear_training_app/static/ear_training_app/exercise.js
@@ -1,25 +1,4 @@
-//needs tones.js, var intervalSetObject
-// function playTopNote(){
-//     tones.play("C#", 5);
-// }
-//
-// function playBottomNote(){
-//     tones.play("A", 4);
-// }
-//
-// function playBothNotes(){
-//     tones.play("C#", 5);
-//     tones.play("A", 4);
-// }
-//
-// function playNote(noteName, octave){
-//     tones.play(noteName, octave);
-// }
-//
-// function playTwoNotes(noteName1, octave1, noteName2, octave2){
-//     tones.play(noteName1, octave1);
-//     tones.play(noteName2, octave2);
-// }
+//needs tones.js
 
 function checkAnswer(event){
     result = document.getElementById("answer-result");
@@ -33,13 +12,16 @@ function checkAnswer(event){
         result.innerHTML = "Incorrect...";
     }
 }
-function init() {
+
+function newExercise() {
+    var intervalSet = JSON.parse(this.responseText);
     var topButton = document.getElementById("top-note-button");
     var bottomButton = document.getElementById("bottom-note-button");
     var bothButton = document.getElementById("both-notes-button");
+    var next = document.getElementById("next");
 
-    for(var i = 0; i < intervalSetObject.length; i++) {
-        var curr = intervalSetObject[i];
+    for(var i = 0; i < intervalSet.length; i++) {
+        var curr = intervalSet[i];
         if(curr.hasOwnProperty("correct")) {
           intervalName = curr.interval_name;
           topNoteName = curr.top_note.name;
@@ -48,7 +30,6 @@ function init() {
           bottomNoteOctave = parseInt(curr.bottom_note.octave);
         }
     }
-
     topButton.addEventListener("click", function() {
         tones.play(topNoteName, topNoteOctave);
     });
@@ -59,14 +40,21 @@ function init() {
         tones.play(topNoteName, topNoteOctave);
         tones.play(bottomNoteName, bottomNoteOctave);
     });
-
     answers = document.getElementsByClassName("interval-button");
     for(var i = 0; i < answers.length; i++){
         answers[i].addEventListener("click", checkAnswer);
     }
+    next.addEventListener("click", nextExercise);
 
     tones.type = "sine";
     tones.release = 200;
+}
+
+function nextExercise(){
+    var request = new XMLHttpRequest();
+    request.onload = newExercise;
+    request.open("GET", "/ear_training_app/get_interval_set", true);
+    request.send();
 }
 
 document.addEventListener("DOMContentLoaded", init);
