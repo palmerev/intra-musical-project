42ea3399697dc5c1723bf5a662980073f516e49b
Evan Palmer <evjpalmer@gmail.com>
2015-08-13 13:49:08 -0700

    wrap whole file in DOMContentLoaded eventListener

--- django/intra_musical_project/ear_training_app/static/ear_training_app/dialogue.js
+++ django/intra_musical_project/ear_training_app/static/ear_training_app/dialogue.js
@@ -1,17 +1,21 @@
-function showDialogue() {
-  document.getElementById("grayout").classList.remove("hidden");
-  document.getElementById("dialogue").classList.remove("hidden");
+function init() {
+    function showDialogue() {
+      document.getElementById("grayout").classList.remove("hidden");
+      document.getElementById("dialogue").classList.remove("hidden");
+    }
+
+    function hideDialogue() {
+      document.getElementById("grayout").classList.add("hidden");
+      document.getElementById("dialogue").classList.add("hidden");
+    }
+
+    var btn = document.getElementById("dialogue-ok");
+    if (btn) {
+        btn.addEventListener("click", hideDialogue);
+    }
+    else {
+        alert("error: dialogue ok-button not found by id");
+    }
 }
 
-function hideDialogue() {
-  document.getElementById("grayout").classList.add("hidden");
-  document.getElementById("dialogue").classList.add("hidden");
-}
-
-var btn = document.getElementById("dialogue-ok");
-if (btn) {
-    btn.addEventListener("click", hideDialogue);
-}
-else {
-    alert("error: dialogue ok button not found by id");
-}
+document.addEventListener("DOMContentLoaded", init);
