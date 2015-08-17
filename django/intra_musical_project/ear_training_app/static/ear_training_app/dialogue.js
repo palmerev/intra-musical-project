a7a7643d6df6b460810d9b16f0ad40f901f6fe7a
Evan Palmer <evjpalmer@gmail.com>
2015-08-17 10:18:02 -0700

    add function makeOverallResults to draw course results in dialogue box

--- django/intra_musical_project/ear_training_app/static/ear_training_app/dialogue.js
+++ django/intra_musical_project/ear_training_app/static/ear_training_app/dialogue.js
@@ -1,21 +1,59 @@
-function init() {
+// function init() {
+    function makeOverallResults(resultsObj) {
+        console.log("MAKING OVERALL RESULTS");
+        console.log(resultsObj);
+        var dialogueCompleted = document.getElementById("ex-completed");
+        var dialogueCorrect = document.getElementById("ex-correct");
+        var dialogueIncorrect = document.getElementById("ex-incorrect");
+        var dialogueSkipped = document.getElementById("ex-skipped");
+        var totalCorrect = 0;
+        var totalIncorrect = 0;
+        var totalSkipped = 0;
+        for (var i = 0; i < resultsObj.length; i++){
+            switch(resultsObj[i]["exercise_result"]){
+              case "correct":
+                totalCorrect++;
+                break;
+              case "incorrect":
+                totalIncorrect++;
+                break;
+              case "skipped":
+                totalSkipped++;
+                break;
+              default:
+                console.log("error: unknown exercise_result!");
+                break;
+            }
+        }
+        var totalComplete = totalCorrect + totalIncorrect;
+        dialogueCompleted.innerHTML = totalComplete;
+        dialogueCorrect.innerHTML = totalCorrect;
+        dialogueIncorrect.innerHTML = totalIncorrect;
+        dialogueSkipped.innerHTML = totalSkipped;
+    }
+
     function showDialogue() {
-      document.getElementById("grayout").classList.remove("hidden");
-      document.getElementById("dialogue").classList.remove("hidden");
+        console.log("SHOWING!");
+        var courseResults = EP.course.studentExercises;
+        var overallResults = makeOverallResults(courseResults);
+        document.getElementById("grayout").classList.remove("hidden");
+        document.getElementById("dialogue").classList.remove("hidden");
     }
 
     function hideDialogue() {
-      document.getElementById("grayout").classList.add("hidden");
-      document.getElementById("dialogue").classList.add("hidden");
+        console.log("HIDING!");
+        document.getElementById("grayout").classList.add("hidden");
+        document.getElementById("dialogue").classList.add("hidden");
     }
 
     var btn = document.getElementById("dialogue-ok");
     if (btn) {
+        console.log("added eventListener to ok-button");
         btn.addEventListener("click", hideDialogue);
     }
     else {
         alert("error: dialogue ok-button not found by id");
     }
-}
+// }
 
-document.addEventListener("DOMContentLoaded", init);
+//document.addEventListener("DOMContentLoaded", init);
