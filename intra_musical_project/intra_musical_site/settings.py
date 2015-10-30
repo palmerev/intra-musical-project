0eaec5a7eff7c3aff73493515e6619663728c80f
Evan Palmer <evjpalmer@gmail.com>
2015-10-30 10:44:24 -0700

    switch from sqlite3 to postgres backend

--- intra_musical_project/intra_musical_site/settings.py
+++ intra_musical_project/intra_musical_site/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'intra_musical_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mydatabase',
-#         'USER': 'mydatabaseuser',
-#         'PASSWORD': 'mypassword',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'intramusical_db',
+        'USER': 'im_admin',
+        'PASSWORD': 'imadminthe*choir01',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
