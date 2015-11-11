4180368e80e4c3d443da5e389c7ea849ed318a02
Evan Palmer <evjpalmer@gmail.com>
2015-11-10 20:25:46 -0800

    refactor models
    add choices to some fields,
    change Integer to Boolean fields in CourseStats,
    rename stuser to student_user

--- intra_musical_project/ear_training_app/models.py
+++ intra_musical_project/ear_training_app/models.py
@@ -3,16 +3,33 @@ from django.contrib.auth.models import User
 
 
 class Note(models.Model):
+    OCTAVE_CHOICES = [(i, i) for i in range(1, 10)]
+
     name = models.CharField(max_length=3, blank=True)
-    octave = models.PositiveSmallIntegerField(default=0)
+    octave = models.PositiveSmallIntegerField(choices=OCTAVE_CHOICES, default=0)
 
     def __str__(self):  # __str__ in python3
         return str(self.name) + str(self.octave)
 
 
 class IntervalType(models.Model):
+    INTERVAL_TYPES = (
+        ('unison', 'unison'),
+        ('minor second', 'minor second'),
+        ('major second', 'major second'),
+        ('minor third', 'minor third'),
+        ('major third', 'major third'),
+        ('perfect fourth', 'perfect fourth'),
+        ('tritone', 'tritone'),
+        ('perfect fifth', 'perfect fifth'),
+        ('minor sixth', 'minor sixth'),
+        ('major sixth', 'major sixth'),
+        ('minor seventh', 'minor seventh'),
+        ('major seventh', 'major seventh'),
+        ('octave', 'octave')
+    )
     '''A list of predefined interval types / qualities'''
-    quality = models.CharField(max_length=20, null=True, blank=True)
+    quality = models.CharField(max_length=20, choices=INTERVAL_TYPES, null=True, blank=True)
 
     def __str__(self):  # __str__ in python3
         return str(self.quality)
@@ -76,7 +93,6 @@ class Course(models.Model):
     ''' Page showing a list of all exercises in a course. '''
     course_type = models.ForeignKey(CourseType, blank=True, null=True)
     num_exercises = models.PositiveSmallIntegerField(default=10, null=True)
-    # exercises = models.ForeignKey(Exercise, null=True, blank=True)
 
     def __str__(self):  # __str__ in python3
         return "course on " + str(self.course_type)
@@ -94,16 +110,22 @@ class Exercise(models.Model):
 
 
 class Student(models.Model):
-    stuser = models.OneToOneField(User, primary_key=True)
+    student_user = models.OneToOneField(User, primary_key=True)
     total_exercises_completed = models.PositiveIntegerField(default=0, null=True, blank=True)
     courses_completed = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):  # __str__ in python3
-        return str(self.stuser)
+        return str(self.student_user)
 
 
 class ExerciseStatus(models.Model):
-    status = models.CharField(max_length=25, default="")
+    STATUSES = (
+        ('skipped', 'skipped'),
+        ('correct', 'correct'),
+        ('incorrect', 'incorrect')
+    )
+
+    status = models.CharField(max_length=25, default="", choices=STATUSES)
 
     def __str__(self):  # __str__ in python3
         return str(self.status)
@@ -124,9 +146,8 @@ class StudentExercise(models.Model):
 class CourseStats(models.Model):
     student = models.ForeignKey(Student)
     course = models.ForeignKey(Course)
-    # Boolean
-    course_complete = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
-    exercises_complete = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
+    course_complete = models.BooleanField(default=False)
+    exercises_complete = models.BooleanField(default=False)
 
     def __str__(self):  # __str__ in python3
         return "CourseStats for " + str(self.course)
